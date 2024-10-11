c0ca741c74f7ebd4a05414ed4afcc1f7d7b7041a
Valdenir Nonaka <159387242+NonakaVal@users.noreply.github.com>
2024-10-11 15:04:15 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import locale
+import streamlit as st
+from utils.GoogleSheetManager import GoogleSheetManager, update_worksheet
+from streamlit_gsheets import GSheetsConnection
+import requests
+# import qrcode
+import io
+
+st.set_page_config(page_title="collectorsguardian", page_icon="📦", layout='wide')
+# Conexão com o Google Sheets
+conn = st.connection("gsheets", type=GSheetsConnection)
+gs_manager = GoogleSheetManager()
+
+locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+def shorten_url_with_requests(url, timeout=10):
+    
+    api_url = f"http://tinyurl.com/api-create.php?url={url}"
+    try:
+        response = requests.get(api_url, timeout=timeout)
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException as e:
+        return f"Erro ao encurtar a URL: {str(e)}"
+# def generate_qr_code(link):
+#     qr_img = qrcode.make(link)
+#     buffer = io.BytesIO()
+#     qr_img.save(buffer, format="PNG")
+#     buffer.seek(0)
+#     return buffer
+# # Função para encurtar todos os links no DataFrame
+def shorten_links_in_df(df, link_column="URL"):
+    df[link_column] = df[link_column].apply(lambda x: shorten_url_with_requests(x))
+    return df
+# URL da planilha Google Sheets
+url = st.secrets["url"]
+
+def norm(data):
+
+    data['SENT'] = data['SENT'].astype(bool)
+    return data
+
+if url:
+    gs_manager.set_url(url)
+    gs_manager.add_worksheet(url, "PRIME")
+    gs_manager.add_worksheet(url, "ESSENCIALS")
+    gs_manager.add_worksheet(url, "LEILÕES")   
+    prime = gs_manager.read_sheet(url, "PRIME")
+    prime = norm(prime)
+    essencials = gs_manager.read_sheet(url, "ESSENCIALS")
+    essencials = norm(essencials)
+    leiloes = gs_manager.read_sheet(url, "LEILÕES")
+    leiloes = norm(leiloes)
+
+# Preparando os dados
+
+def get_link(data):
+    data['URL'] = data.apply(lambda row: f"https://www.collectorsguardian.com.br/{row['ITEM_ID'][:3]}-{row['ITEM_ID'][3:]}-{row['TITLE'].replace(' ', '-').lower()}-_JM#item_id={row['ITEM_ID']}", axis=1)
+    data = data[['ITEM_ID', 'TITLE', 'URL', "DATA",  'SENT' ]]
+
+    data = shorten_links_in_df(data)
+    return data
+
+# prime = get_link(prime)
+
+# essencials = get_link(essencials)
+
+st.subheader("PRIME")
+update_worksheet(prime, "PRIME", 5, url)
+# st.dataframe(prime, column_config={"URL": st.column_config.LinkColumn("Links", display_text="Acessar anúncio")})
+st.data_editor(prime)
+
+
+
+
+# # Gerando os QR Codes e exibindo no Streamlit
+# st.subheader("QR Codes para os Links Encurtados")
+# for index, row in prime.iterrows():
+#     st.write(f"Item: {row['TITLE']}")
+#     st.write(f"Link encurtado: {row['URL']}")
+    
+#     # Gerando QR Code
+#     qr_code_img = generate_qr_code(row['URL'])
+    
+#     # Exibindo a imagem do QR Code
+#     st.image(qr_code_img, caption=row['TITLE'], use_column_width=False)
+
+
+
+st.subheader("ESSENCIALS")
+# st.dataframe(essencials, column_config={"URL": st.column_config.LinkColumn("Links", display_text="Acessar anúncio")})
+# # st.dataframe(prime, column_config={"URL": st.column_config.LinkColumn("Links", display_text="Acessar anúncio")})
+update_worksheet(essencials, "ESSENCIALS", 100,url)
+st.data_editor(essencials)
+
+# # Gerando os QR Codes e exibindo no Streamlit
+# st.subheader("QR Codes para os Links Encurtados")
+# for index, row in essencials.iterrows():
+#     st.write(f"Item: {row['TITLE']}")
+#     st.write(f"Link encurtado: {row['URL']}")
+    
+#     # Gerando QR Code
+#     qr_code_img = generate_qr_code(row['URL'])
+
+#     # Exibindo a imagem do QR Code
+#     st.image(qr_code_img, caption=row['TITLE'], use_column_width=False)
+
+
+st.subheader("LEILÕES")
+update_worksheet(leiloes, "LEILÕES", 6, url)
+# st.dataframe(leiloes, column_config={"URL": st.column_config.LinkColumn("Links", display_text="Acessar anúncio")})
+st.data_editor(leiloes)
---
+++ utils/GoogleSheetManager.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import streamlit as st
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+def update_worksheet(df, worksheet_title, key, url):
+    if st.button("Update Worksheet",key=key):
+        # url = st.secrets["url"]
+        conn.update(spreadsheet=url, worksheet=worksheet_title, data=df)
+        st.success("Worksheet Updated 🤓")
+
+
+class GoogleSheetManager:
+    def __init__(self, connection_type="gsheets"):
+        self.conn = st.connection(connection_type, type=GSheetsConnection)
+        self.spreadsheets = {}  # Dicionário para armazenar URLs e suas worksheets
+
+    def set_url(self, url):
+        """Define a URL de uma planilha e inicializa suas worksheets."""
+        if url not in self.spreadsheets:
+            self.spreadsheets[url] = []  # Cria uma lista para armazenar as worksheets
+
+    def add_worksheet(self, url, worksheet_name):
+        """Adiciona uma nova worksheet à planilha especificada pela URL."""
+        if url in self.spreadsheets:
+            self.spreadsheets[url].append(worksheet_name)
+        else:
+            st.error("URL não encontrada. Adicione a URL primeiro.")
+
+    def read_sheet(self, url, worksheet):
+        """Lê dados de uma worksheet específica de uma URL."""
+        if url in self.spreadsheets and worksheet in self.spreadsheets[url]:
+            data = self.conn.read(spreadsheet=url, worksheet=worksheet)
+            return data.copy()
+        else:
+            st.error("Worksheet não encontrada para esta URL.")
+            return None
+
+    def update_sheet(self, url, worksheet, data):
+        """Atualiza uma worksheet específica de uma URL."""
+        if url in self.spreadsheets and worksheet in self.spreadsheets[url]:
+            self.conn.update(spreadsheet=url, worksheet=worksheet, data=data)
+            st.success("Worksheet Atualizada 🤓")
+        else:
+            st.error("Worksheet não encontrada para esta URL.")
+
+
